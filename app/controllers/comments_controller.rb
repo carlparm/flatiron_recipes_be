769656ad6296ed215845f6e06cfd9a2ee278ca11
Carl <carlparm@gmail.com>
2020-08-05 17:28:30 -0400

    controller setup

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+
+    def index
+        comments = Comment.all
+        render json: comments.to_json()
+    end
+
+    def create
+        comment = Comment.create(user_id: 34, recipe_id: params[:recipe_id], content: params[:content])
+        render json: comment.to_json()
+    end
+    
 end
